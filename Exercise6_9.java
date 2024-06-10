3290a92751c22ff04cb2824dfe5b5486612f8ed9
JCBIvy <157339653+JCBIvy@users.noreply.github.com>
2024-06-10 02:49:53 +0000

    Finished Program

--- Exercise6_9.java
+++ Exercise6_9.java
@@ -18,20 +18,39 @@ class Exercise6_9
 
     public static void main(String []args)
     {
-        //create the set of values used in the test
-        //each item in the array represents one part of row of the table
-        //the first number in each row corresponds to the feet in the left column in the table, the second to the meters in the right column
-        double[][] test_values = new double[10][2];
+        //create & process the set of values used in the test
+        //each item in the array represents one row of the table
+        double[][] test_values = new double[10][4];
         for (int i=0; i<10; i++)
         {
+            //the numbers are the values listed in the order they will be printed in the table
             test_values[i][0] = i+1;
-            test_values[i][1] = i*5+20;
+            test_values[i][1] = footToMeter(test_values[i][0]);
+            test_values[i][2] = i*5+20;
+            test_values[i][3] = meterToFoot(test_values[i][2]);
         }
 
         //create the header for the table
+        System.out.println("Feet      Meters    |  Meters    Feet      ");
+        System.out.println("--------------------+----------------------");
         
+        //create the table
+        //build it one row at a time
+        for (int i=0; i<10; i++)
+        {
+            //create an output string
+            String output = "";
 
-        String output = String.format("%-10s", "foo") + "b";
-        System.out.println(output);
+            //place all the numbers into the output string, appropriately padded
+            output += String.format("%-10s", test_values[i][0]);
+            output += String.format("%-10s", test_values[i][1]);
+            output += "|  ";
+            output += String.format("%-10s", test_values[i][2]);
+            output += String.format("%-10s", test_values[i][3]);
+
+            //print the output for this row
+            System.out.println(output);
+        }
+
     }
 };
